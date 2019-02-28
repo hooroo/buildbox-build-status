5a4086db56f1f15f68177367bc0cb87adecff707
allanpope <pope.allans@gmail.com>
2019-02-28 15:24:46 +1100

    bump packages

--- public/scripts/buildkite.js
+++ public/scripts/buildkite.js
@@ -53,5 +53,3 @@ var buildkite = {
   }
 
 }
-
-module.exports = buildkite
--- public/scripts/micro_templates.js
+++ public/scripts/micro_templates.js
@@ -4,11 +4,11 @@
   var cache = {};
 
   this.tmpl = function tmpl(str, data){
-    console.log('str', document.getElementById(str));
     // Figure out if we're getting a template, or if we need to
     // load the template - and be sure to cache the result.
     var fn = !/\W/.test(str) ?
       cache[str] = cache[str] ||
+        tmpl(document.getElementById(str).innerHTML) :
 
       // Generate a reusable function that will serve as a template
       // generator (and which will be cached).
